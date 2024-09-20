257b7f5664379507b77be76e816eb5509ba7bcc3
Shyam_0373 <shyamtripathi373@gmail.com>
2024-09-20 11:45:04 +0530

    ger tuserBy email function is added in utils action.ts

--- utils/db/action.ts
+++ utils/db/action.ts
@@ -11,3 +11,13 @@ export async function createUser(email: string, name: string) {
     return null;
   }
 }
+
+export async function getUserByEmail(email: string) {
+    try {
+      const [user] = await db.select().from(Users).where(eq(Users.email, email)).execute();
+      return user;
+    } catch (error) {
+      console.error("Error fetching user by email:", error);
+      return null;
+    }
+  }
